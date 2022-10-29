59ae4104c8a66ef476c99a963a85ff5d2b29cbab
nazarpunk <nazarpunk@gmail.com>
2022-10-29 05:39:05 +0300

    v 1.0

--- src/game.mjs
+++ src/game.mjs
@@ -13,7 +13,6 @@ class Button {
 	}
 }
 
-
 class Child {
 	/**
 	 * @param {number} id
@@ -25,7 +24,6 @@ class Child {
 		this.text = text;
 		this.buttons = [];
 	}
-
 }
 
 
@@ -35,18 +33,41 @@ class Game {
 	 * @param {string} path
 	 */
 	constructor(container, path) {
-		this.container = document.createElement('div');
-		container.appendChild(this.container);
 		this.path = path;
+
+		this.gameDiv = document.createElement('div');
+		this.gameDiv.classList.add('game');
+		this.gameDiv.style.display = 'none';
+		container.appendChild(this.gameDiv);
+
+		this.visDiv = document.createElement('div');
+		this.visDiv.classList.add('vis');
+		container.appendChild(this.visDiv);
+
+		this.nodes = new vis.DataSet();
+		this.edges = new vis.DataSet();
+
+		this.vis = new vis.Network(
+			this.visDiv,
+			{
+				nodes: this.nodes,
+				edges: this.edges,
+			}, {
+				layout: {
+					improvedLayout: false
+				}
+			}
+		);
+
+		this.converter = new showdown.Converter();
+
+		this.step = this.step.bind(this);
 	}
 
-	/** @type {HTMLElement} */
-	container;
+	/** @type {HTMLElement} */ gameDiv;
+	/** @type {HTMLElement} */ visDiv;
 
-	/** @type {Map<number, Child>} */
-	childs = new Map();
-	nodes = [];
-	edges = [];
+	/** @type {Map<number, Child>} */ childs = new Map();
 
 	/**
 	 * @private
@@ -54,54 +75,86 @@ class Game {
 	 */
 	parse(text) {
 		let index = -1;
-		let content = ``;
-		/** @type {Button[]} */
-		let buttons = [];
 
-		const save = () => {
-			this.nodes.push({id: index, label: `${index}`});
+		const complete = () => {
+			const child = this.childs[index];
 
-			this.childs[index] = new Child(index,content.trim(),buttons);
-			content = ``;
+			child.text = child.text.trim();
 
-			for (const button of buttons) {
-				button.text = button.text.trim();
-				this.edges.push({from: index, to: button.id, arrows: `to`});
+			const matches = child.text.matchAll(/\[\d+]\(#(\d+)\)/g);
+			for (const match of matches) {
+				const to = +match[1];
+				const id = `${index}-${to}`;
+
+				if (!this.edges.get(id)) {
+					this.edges.add({id: id, from: index, to: to, arrows: `to`});
+				}
 			}
 
-			buttons = [];
+			child.text = this.converter.makeHtml(child.text);
+
+			this.nodes.add({id: index, label: `${index}`});
+			for (const button of this.childs[index].buttons) {
+				button.text = this.converter.makeHtml(button.text.trim());
+				this.edges.add({from: index, to: button.id, arrows: `to`});
+			}
+
 		};
 
 		for (let str of text.split(rNewLine)) {
 			if (rBlock.test(str)) {
-				if (index >= 0) save();
+				if (index >= 0) complete();
 				index = +str.match(rBlock)[1];
+				this.childs[index] = new Child(index, '', []);
 				continue;
 			}
+
+			const child = this.childs[index];
 
 			if (rButton.test(str)) {
 				const [, id] = str.match(rButton);
-				buttons.push(new Button(+id, ''));
+				child.buttons.push(new Button(+id, ''));
 				continue;
 			}
 
-			if (buttons.length) {
-				buttons[buttons.length - 1].text += '\n' + str;
+			if (child.buttons.length) {
+				child.buttons[child.buttons.length - 1].text += '\n' + str;
 			} else {
-				content += '\n' + str;
+				child.text += '\n' + str;
 			}
-
 		}
-		save();
-
-		console.log(this.childs);
+		complete();
 	}
 
+	step() {
+		let id = +window.location.hash.replace(/\D+/, ``);
+		id = isNaN(id) ? 0 : id;
+
+		if (!this.childs.hasOwnProperty(id)) {
+			//alert(`Paragraph is missing: ${id}`);
+			return;
+		}
+		this.gameDiv.innerHTML = this.childs[id].text.trim();
+		this.gameDiv.querySelectorAll(`p:empty`).forEach(p => p.remove());
+
+		for (const button of this.childs[id].buttons) {
+			this.gameDiv.insertAdjacentHTML(`beforeend`, `<a href="#${button.id}" class="button">${button.text}</a>`);
+		}
+
+	};
 
 	async start() {
 		const request = await fetch(this.path);
 		const response = await request.text();
 		this.parse(response);
+
+		//this.vis.setData({nodes: this.nodes, edges: this.edges});
+
+		addEventListener(`hashchange`, this.step);
+		this.step();
+		this.gameDiv.style.removeProperty('display');
+
+		document.querySelectorAll('.loader').forEach(e => e.remove());
 	}
 }
 
--- src/main.mjs
+++ src/main.mjs
@@ -1,35 +1,4 @@
 import {Game} from './game.mjs';
 
 const game = new Game(document.body, '/book/rat.md');
-game.start();
-
-
-if (0) {
-	const step = () => {
-		let id = +window.location.hash.replace(/\D+/, ``);
-		id = isNaN(id) ? 0 : id;
-
-		if (!data.hasOwnProperty(id)) {
-			//alert(`Paragraph is missing: ${id}`);
-			return;
-		}
-		$main.innerHTML = data[id].text.trim();
-		$main.querySelectorAll(`p:empty`).forEach($p => $p.remove());
-
-		for (const btn of data[id].buttons) {
-			$main.insertAdjacentHTML(`beforeend`, `<a href="#${btn[0]}" class="button">${btn[1]} <span>${btn[0]}</span></a>`);
-		}
-
-	};
-
-	addEventListener(`hashchange`, step);
-	step();
-
-
-	if (0) {
-		new vis.Network(document.getElementById('vis'), {
-			nodes: new vis.DataSet(nodes),
-			edges: new vis.DataSet(edges)
-		}, {});
-	}
-}
+game.start().then(_ => {});
